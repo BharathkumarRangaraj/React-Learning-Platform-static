eebc9b6281cd20a4936320b11972bb8014d477e2
BharathkumarRangaraj <104580107+BharathkumarRangaraj@users.noreply.github.com>
2022-05-19 20:56:40 +0530

    Create Nav.js

---
+++ Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navv() {
+  return (
+    <div className="Navbar">
+      <img className="Reactimage" src="./logo192.png" />
+      <h2 className="Reactfacts">ReactFacts </h2>
+      <h3 className="Reactcourse">React Course </h3>
+    </div>
+  );
+}
