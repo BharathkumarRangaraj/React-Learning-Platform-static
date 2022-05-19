9023924a8027eb32a479ec6ee53a8e397d84c5e9
BharathkumarRangaraj <104580107+BharathkumarRangaraj@users.noreply.github.com>
2022-05-19 20:57:02 +0530

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Mainn() {
+  return (
+    <div className="mainsection">
+      <h1 className="maincontent">Fun facts about React</h1>
+      <ul className="listofcontent">
+        <li>was first released in 2013</li>
+        <li>was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thouands of enterprise apps,including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
