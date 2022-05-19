050e3b0ee91b78188fde327534114d208554d0ca
BharathkumarRangaraj <104580107+BharathkumarRangaraj@users.noreply.github.com>
2022-05-19 20:57:25 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p className="Footertext"> &copy; 2022 reserved by Bharath</p>
+    </div>
+  );
+}
